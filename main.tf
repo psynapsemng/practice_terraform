a9d41b35480b85dc91322f5fc00c4bef668f7a98
ManhNG <manguyen.hoa@gmail.com>
2023-02-07 15:07:23 +0100

    add_web_server_configurable

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ provider "aws" {
 
 resource "aws_instance" "web_server" {
   ami           = "ami-0a89a7563fc68be84"
-  instance_type = "t2.micro"
+  instance_type = var.instancetype
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -37,9 +37,9 @@ resource "aws_security_group" "instance" {
 
   ingress {
     cidr_blocks = [ "0.0.0.0/0" ]
-    from_port = 8080
+    from_port = var.server_port
     protocol = "tcp"
-    to_port = 8080
+    to_port = var.server_port
   } 
 }
 
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "server_port" {
+  description = "The port the server sill use for HTTP requests"
+  type = number
+  default = "8080"
+}
+
+variable "instancetype" {
+  type = string
+  description = "set aws instance type"
+  default = "t2.nano"
+}
