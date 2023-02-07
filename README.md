70f1a100e6ba1f57031bfc8c7b330d834ecb1e80
psynapsemng <78550822+psynapsemng@users.noreply.github.com>
2023-02-07 12:46:24 +0100

    Add gmain et read me

---
+++ README.md
@@ -0,0 +1,2 @@
+# practice_terraform
+# practice_terraform
---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0a89a7563fc68be84"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+    Name = "WebServerInstance"
+  }
+}
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+  } 
+}
+
+variable "security_group_name" {
+  description = "The name of the security group"
+  type = string
+  default = "terraform-exemple-instance"
+  
+}
+
+output "public-ip" {
+  value = aws_instance.web_server.public_ip
+  description = "The public IP of the instance"
+}
