63e7b4d71c883d7e458f83fda34ef26fac977a1b
ManhNG <manguyen.hoa@gmail.com>
2023-02-08 21:17:49 +0100

    lab_005

--- main.tf
+++ main.tf
@@ -13,134 +13,38 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running state"
 
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-0a89a7563fc68be84"
-  instance_type = var.instancetype
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Helllo,World" > index.html
-              nuhup busybox httpd -f -p ${var.server_port} &
-              EOF
-  
-  # Required when using a launch configuration with an auto scaling group.
-  # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+  # Prevent accidental deletion of this S3 bucket
   lifecycle {
-    create_before_destroy = true
+    prevent_destroy = true
   }
-}
 
-# Définit l'application load balancer
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
 
-resource "aws_lb" "alb" {
-  name = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets = data.aws_subnet_ids.default.ids
-  # raccrocher ALB au groupe de sécurité
-  security_groups = [aws_security_group.firewall.id]
-}
-
-# Définit un écouteur  pour l'ALB
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.alb.arn
-  port = 80
-  protocol = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code = 404
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
     }
   }
 }
 
-# définit security_group pour entrées/sorties
+resource "aws_dynamodb_table" "terraform_locks" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
 
-resource "aws_security_group" "firewall" {
-  name = "terraform-example-firewall"
-
-  # allow inbound HTTP requests
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # allow all outbound requests
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
-
-# Créer un groupe cibble pour l'ASG
-
-resource "aws_lb_target_group" "asg" {
-  name = "terraform-asg-example"
-  port = var.server_port
-  protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
-
-  health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet_ids.default.ids
-  
-  # définit son argument target_group_arns pour pointer vers le nouveau groupe ciblé
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 5
-
-  tag {
-    key = "Name"
-    value = "terraform-asg-exemple"
-    propagate_at_launch = true
-  }
-}
-
-# ajoute une règle d'écouteur (listening) qui envoie des requêtes
-# correspondant à n'importe quel chemin d'accès au groupe cible qui contient l'ASG
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
-}
-
-
-
-
-
-
 
